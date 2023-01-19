302e221f130e550dc68a97c8b01a35cf23d63324
ajlaroya <arthur.j.laroya@gmail.com>
2023-01-19 20:36:02 +1100

    medium/large card, footer

---
+++ components/Footer.jsx
@@ -0,0 +1,45 @@
+function Footer() {
+  return (
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-4 sm:grid-cols-2 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">ABOUT</h5>
+          <p>How Fiesta works</p>
+          <p>Newsroom</p>
+          <p>Investors</p>
+          <p>Fiesta Plus</p>
+          <p>Fiesta Luxe</p>
+        </div>
+
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">COMMUNITY</h5>
+          <p>Accessibility</p>
+          <p>About Us</p>
+          <p>Press</p>
+          <p>Relief</p>
+          <p></p>
+        </div>
+
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">HOST</h5>
+          <p>Fiesta your home</p>
+          <p>FiestaCover</p>
+          <p>Host resources</p>
+          <p>Forum</p>
+          <p>Host responsiblity</p>
+        </div>
+
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">SUPPORT</h5>
+          <p>Help Centre</p>
+          <p>Disability support</p>
+          <p>Cancellation</p>
+          <p>COVID-19 Response</p>
+          <p>Neighbourhood concerns</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,10 +3,13 @@ import { Inter } from "@next/font/google";
 import Header from "../components/Header";
 import Banner from "@/components/Banner";
 import SmallCard from "@/components/SmallCard";
+import MediumCard from "@/components/MediumCard";
+import LargeCard from "@/components/LargeCard";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home({ exploreData }) {
+export default function Home({ exploreData, cardsData }) {
   return (
     <div className={inter.className}>
       <Head>
@@ -35,8 +38,23 @@ export default function Home({ exploreData }) {
 
         <section>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
         </section>
+
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Wishlists curated by Airbnb"
+          buttonText="Get inspired"
+        />
       </main>
+
+      <Footer />
     </div>
   );
 }
@@ -46,9 +64,14 @@ export async function getStaticProps() {
     (res) => res.json()
   );
 
+  const cardsData = await fetch("https://www.jsonkeeper.com/b/VHHT").then(
+    (res) => res.json()
+  );
+
   return {
     props: {
       exploreData,
+      cardsData,
     },
   };
 }
